cbf547e5086e8d19587cabd865bbb1e56f9b404d
pegasus-lynx <dipeshkrj14@gmail.com>
2018-10-25 03:51:41 +0530

    Views and urls in home

--- GenMed/home/views.py
+++ GenMed/home/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import MySQLdb
+
+def connect():
+    return MySQLdb.connect(user="root",passwd="DKumar@14",db="bank")
 
 # Create your views here.
+
+def home(request):
+    context = {}
+    return render(request, 'home/homepage.html', context)
+
+def login(request):
+    if request.method=='POST':
+        db=connect()
+        c=db.cursor()
+        c.execute(
+            """ SELECT * from """,
+            ()
+        )
+
+        
+        return HtteResponse("Added!")
+    else:
+        context = {}
+        return render(request, 'home/login-page.html', context)
+
+def signup(request):
+    if request.method=='POST':
+        db=connect()
+        c=db.cursor()
+        # extract field information
+        c.execute(
+            """ INSERT into """,
+            ()
+        )
+        c.execute(
+            """ INSERT into """,
+            ()
+        )
+        c.execute(
+            """ INSERT into """,
+            ()
+        )
+        db.commit()
+        return HtteResponse("Added!")
+    else:
+        context = {}
+        return render(request, 'home/signup-page.html', context)
+
